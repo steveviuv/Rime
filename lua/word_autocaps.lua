e86717acd6aae3846b74691400c28bec7a81390c
Viuv <steve_via@qq.com>
2025-12-20 16:53:58 +0800

    阶段2

--- lua/word_autocaps.lua
+++ lua/word_autocaps.lua
@@ -1,47 +1,48 @@
--- rime.lua
--- engword_autocaps = require("word_autocaps")
--- engword_autocaps_filter = engword_autocaps.filter
--- engword_autocaps_translator = engword_autocaps.translator
-
 -- 输入的内容大写前2个字符，自动转小写词条为全词大写；大写第一个字符，自动转写小写词条为首字母大写
--- local puts = require("tools/debugtool")
 
 local function autocap_filter(input, env)
+    local u_cands = {}
     for cand in input:iter() do
         local text = cand.text
-        local commit = env.engine.context:get_commit_text()
-        if string.find(text, "^%l%l.*") and string.find(commit, "^%u%u.*") then
+        local preedit_code = env.engine.context:get_commit_text()
+        if string.find(text, "^%l%l.*") and string.find(preedit_code, "^%u%u.*") then
             if string.len(text) == 2 then
-                local cand_2 = Candidate("cap", 0, 2, commit, "+")
+                ---@diagnostic disable-next-line: missing-parameter
+                local cand_2 = ShadowCandidate(cand, cand.type, preedit_code, "+")
                 yield(cand_2)
             else
-                local cand_u = Candidate("cap", 0, string.len(commit), string.upper(text), "+AU")
-                yield(cand_u)
+                local cand_u = Candidate("cap", 0, preedit_code:len(), text:upper(), "+AU")
+                table.insert(u_cands, cand_u)
             end
-        --[[ 修改候选的注释 `cand.comment`
-            因复杂类型候选项的注释不能被直接修改，
-            因此使用 `get_genuine()` 得到其对应真实的候选项
-            cand:get_genuine().comment = cand.comment .. " " .. s
-        --]]
-        elseif string.find(text, "^%l+$") and string.find(commit, "^%u+") then
-            local suffix = string.sub(text, string.len(commit) + 1)
-            local cand_ua = Candidate("cap", 0, string.len(commit), commit .. suffix, "+" .. suffix)
-            yield(cand_ua)
-        --[[ elseif string.find(text, "^%l+$") and (not string.find(text, "[:/.@]+")) then
-            local cand_as = Candidate("as", 0, string.len(text)+2, text .. " " , "~AS")
-            yield(cand_as) ]]
+        elseif string.find(text, "^%l+$") and string.find(preedit_code, "^%u+") then
+            local suffix = string.sub(text, string.len(preedit_code) + 1)
+            ---@diagnostic disable-next-line: missing-parameter
+            local cand_t = ShadowCandidate(cand, cand.type, preedit_code .. suffix, "~AT")
+            table.insert(u_cands, cand_t)
         else
             yield(cand)
         end
+
+        if #u_cands >= 200 then break end
+    end
+
+    for _, cand in ipairs(u_cands) do
+        yield(cand)
     end
 end
 
 ---@diagnostic disable-next-line: unused-local
 local function autocap_translator(input, seg, env)
-    if string.match(input, '%u%u%l+') then
-        local cand = Candidate("word_caps", seg.start, seg._end, string.upper(input), '~AU')
+    if input:match("^%u%l%l?%l?%l?%l?%l?%l?%l?%l?%l?%l?$") and input:match("^[^V].*") then
+        local cand = Candidate("Word", seg.start, seg._end, input, "")
         yield(cand)
+    elseif input:match("^%u%u%a+$") then
+        local new_txt = input:upper()
+        yield(Candidate("word_caps", seg.start, seg._end, new_txt, "~AU"))
+        -- elseif input:match("^%u%l+%u%a?") then
+    elseif input:match("^%a+[.:_-]%/?%/?%/?%a+") then
+        yield(Candidate("en_custom", seg.start, seg._end, input, "~AC"))
     end
 end
 
-return {filter = autocap_filter, translator = autocap_translator }
+return { filter = autocap_filter, translator = autocap_translator }
